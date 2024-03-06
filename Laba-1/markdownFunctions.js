037478b6dbc59b50a82903316cb1ccac5e6a38d7
makskhv21 <khvits21@gmail.com>
2024-03-06 17:08:41 +0200

    feat: Add functions for extracting, insertingCodeBlocks

--- Laba-1/markdownFunctions.js
+++ Laba-1/markdownFunctions.js
@@ -30,3 +30,25 @@ function validateMarkdownSymbols(markdownText) {
       throw new Error(`Unfinished markdown symbol - ${words.find(word => markdownSymbols.some(symbol => isUnfinishedSymbol(word, symbol)))}`);
     }
 }
+
+// This function extracts code blocks enclosed in triple backticks from the given text and populates the provided array.
+function extractCodeBlocks(text, codeBlocks) {
+    const codeBlockPlaceholder = '%%CODE_BLOCK%%';
+    return text.replace(/```([\s\S]*?)```/g, (match, content) => {
+        codeBlocks.push(content);
+        return codeBlockPlaceholder;
+    });
+}
+  
+// This function inserts code blocks back into the text using a placeholder and the provided array.
+function insertCodeBlocks(text, codeBlocks) {
+    const codeBlockPlaceholder = '%%CODE_BLOCK%%';
+    return text.replace(new RegExp(codeBlockPlaceholder, 'g'), () => '<pre>' + codeBlocks.shift() + '</pre>');
+}
+  
+module.exports = {
+    validateHTMLStructure,
+    validateMarkdownSymbols,
+    extractCodeBlocks,
+    insertCodeBlocks,
+};
