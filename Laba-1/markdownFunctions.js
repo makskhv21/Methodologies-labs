43deae69ca9f0c02b3c2a593dca460c05311fa22
makskhv21 <khvits21@gmail.com>
2024-03-06 17:04:08 +0200

    feat: Add Markdown symbol validation function

--- Laba-1/markdownFunctions.js
+++ Laba-1/markdownFunctions.js
@@ -1,5 +1,6 @@
 'use strict'
 
+// This function validates the HTML structure within Markdown text.
 function validateHTMLStructure(markdownText) {
   const sanitizedTags = (markdownText.match(/<\/?[^>]+>/g) || []).map(tag => tag.replace('/', ''));
 
@@ -8,4 +9,24 @@ function validateHTMLStructure(markdownText) {
       throw new Error("Mismatched HTML tags. Check for nesting issues.");
     }
   }
+}
+
+// This function validates if markdown symbols in the given text are properly closed and not unfinished.
+function validateMarkdownSymbols(markdownText) {
+    // List of supported markdown symbols to check for completeness.
+    const markdownSymbols = ['**', '_', '`'];
+    
+    const isUnfinishedSymbol = (word, symbol) =>
+      word.includes(symbol) && word !== symbol &&
+      (word.startsWith(symbol) || word.endsWith(symbol));
+  
+    const hasUnfinishedSymbol = words => words.some(word =>
+      markdownSymbols.some(symbol => isUnfinishedSymbol(word, symbol))
+    );
+  
+    const words = markdownText.split(/\s+/);
+  
+    if (hasUnfinishedSymbol(words)) {
+      throw new Error(`Unfinished markdown symbol - ${words.find(word => markdownSymbols.some(symbol => isUnfinishedSymbol(word, symbol)))}`);
+    }
 }
